b84dea6484f34abb49189b989a494a46c69617ec
Yermehyaw <yermehyaw@gmail.com>
2026-09-17 18:32:18 +0100

    [Fix]: db missing leads to /var/instance crash

--- config/config.py
+++ config/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'medisec-super-secret-hackathon-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///medisec.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:////tmp/medisec-instance/medisec.db'  # Replace /tmp/... when SQLlite db is created
     SQLALCHEMY_TRACK_MODIFICATIONS = False
